9c1493eba22d9e0b67de1dcd628e5ac201ae79ce
Yoshioka <sss.yoshioka@gmail.com>
2021-02-09 17:40:03 +0900

    annotate有効化

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  uid        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
 end
